81b00161a47e01ca731d72a287908ed4e6e283b2
Goopy25 <raylee25@proton.me>
2024-10-07 14:29:52 -0800

    added all races and most subraces

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NPC Generator II
 Generates a random D&D NPC with a ludicrous amount of detail. Height, weight, age, eye color... History!! *Everything!!*
 ## Races Implemented:
-11.5/48
+45.0/46
 - Aarakocra ✔
 - Aasimar ✔
 - Autognome ✔
@@ -14,38 +14,56 @@ Generates a random D&D NPC with a ludicrous amount of detail. Height, weight, ag
 - Fairy ✔
 - Firbolg ✔
 - Genasi ✔
-- Giff
-- Gith
-- Gnome
-- Goblin
-- Goliath
-- Grung
-- Hadozee
-- Half-Elf
-- Half-Orc
-- Halfling
-- Harengon
-- Hobgoblin
-- Human
-- Kalashtar
-- Kender
-- Kenku
-- Kobold
-- Leonin
-- Lizardfolk
-- Locathah
-- Loxodon
-- Minotaur
-- Orc
-- Owlin
-- Plasmoid
-- Satyr
-- Shifter
-- Simic Hybrid
-- Tabaxi
-- Thri-kreen
-- Tiefling
-- Tortle
-- Veldalken
-- Warforged
-- Yuan-Ti Pureblood
+- Giff ✔
+- Gith ✔
+- Gnome ✔
+- Goblin ✔
+- Goliath ✔
+- Grung ✔
+- Hadozee ✔
+- Half-Elf ✔ (Partially)
+- Half-Orc ✔
+- Halfling ✔
+- Harengon ✔
+- Hobgoblin ✔
+- Human ✔
+- Kalashtar ✔
+- Kenku ✔
+- Kobold ✔
+- Leonin ✔
+- Lizardfolk ✔
+- Locathah ✔
+- Loxodon ✔
+- Minotaur ✔
+- Orc ✔
+- Owlin ✔
+- Plasmoid ✔
+- Satyr ✔
+- Shifter ✔
+- Simic Hybrid ✔
+- Tabaxi ✔
+- Thri-kreen ✔
+- Tiefling ✔
+- Tortle ✔
+- Veldalken ✔
+- Warforged ✔
+- Yuan-Ti Pureblood ✔
+## Sources:
+Height & Weight: https://blackcitadelrpg.com/height-age-weight-5e/
+*Player's Handbook* (2014)
+*The Wild Beyond the Witchlight*
+*Volo's Guide to Monsters*
+*Mythic Odysseys of Theros*
+*Spelljammer: Adventures in Space*
+*Locathah Rising*
+*One Grung Above*
+*Wayfinder's Guide to Eberron*
+*Guildmaster's Guide to Ravnica*
+*Strixhaven: A Curriculum of Chaos*
+*Elemental Evil Player's Companion*
+*The Tortle Package*
+*Fizban's Treasury of Dragons*
+*Mordenkainen's Tome of Foes*
+*Sword Coast Adventurer's Guide*
+The Forgotten Realms Fandom Wiki: https://forgottenrealms.fandom.com
+The Magic: The Gathering Fandom Wiki: https://mtg.fandom.com
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import chalk from "chalk";
 import races from "./races.json" with {type: "json"};
-import subraces from "./subraces.json" with {type: "json"};
-// import * as rpgDiceRoller from '@dice-roller/rpg-dice-roller';
 import { DiceRoll } from "@dice-roller/rpg-dice-roller";
 
 function getRace() {
     let randomRaceIndex = Math.floor(Math.random() * races.length);
     let race = races[randomRaceIndex]
     let age = Math.floor(Math.random() * race.dies);
+    let lifestage = "adult";
+    if (age >= race.dies / 2) {
+        lifestage = "elder";
+    } else if (age <= race.matures) {
+        lifestage = "child";
+    }
     let subrace = race.subraces[Math.floor(Math.random() * race.subraces.length)];
     let eyeColor = subrace.eyeColors[Math.floor(Math.random() * subrace.eyeColors.length)];
     let hairColor = subrace.hairColors[Math.floor(Math.random() * subrace.hairColors.length)];
@@ -18,17 +22,44 @@ function getRace() {
     let heightFeet = Math.floor(heightTotalInches / 12);
     let heightInches = Math.abs((heightFeet * 12) - heightTotalInches);
     let weight = subrace.baseWgt + (hgtMod.total * wgtMod.total);
-    console.log(
-        `
-        Race: ${race.name}, (${subrace.name})
-        Age: ${age}
-        Eyes: ${eyeColor}
-        Hair: ${hairColor}
-        Skin: ${skinColor}
-        Height: ${heightFeet}'${heightInches}"
-        Weight: ${weight} lb.
-        `
-    );
+    if (race.subraces.length > 1) {
+        console.log(`${chalk.inverse(`Race: ${race.name}, (${subrace.name})`)}`)
+    } else {
+        console.log(`${chalk.inverse(`Race: ${race.name}`)}`)
+    }
+    console.log(`Alignment: ${getAlignment()}\nAge: ${age} (${lifestage})\nEyes: ${eyeColor}\nHair: ${hairColor}\nSkin: ${skinColor}\nHeight: ${heightFeet}'${heightInches}"\nWeight: ${weight} lb.`);
+}
+
+function getAlignment() {
+    let roll = new DiceRoll("3d6");
+    let tiebreaker = new DiceRoll("1d100");
+    if (roll.total >= 18) {
+        if (tiebreaker.total < 50) {
+            return "chaotic good";
+        } else {
+            return "chaotic neutral";
+        }
+    } else if (roll.total >= 16) {
+        if (tiebreaker.total < 50) {
+            return "lawful good";
+        } else {
+            return "lawful neutral";
+        }
+    } else if (roll.total >= 13) {
+        return "neutral good";
+    } else if (roll.total >= 9) {
+        return "neutral";
+    } else if (roll.total >= 6) {
+        return "neutral evil";
+    } else if (roll.total >= 4) {
+        return "lawful evil";
+    } else {
+        if (tiebreaker.total < 50) {
+            return "chaotic evil";
+        } else {
+            return "chaotic neutral";
+        }
+    }
 }
 
 getRace();
